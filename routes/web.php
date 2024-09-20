041da3ef6be307b1602bd0e71c245f35dbdb0c86
MaharaniWahyuTantri <maharaniwahyutantri525@gmail.com>
2024-09-20 20:39:19 +0700

    tugas3

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+  //  return view('welcome');
+//});
+
+//Route::get('/user/profile', [UserController::class, 'profile']);
+
+Route::get('user/create', [UserController::class, 'create']);
+
+Route::get('user/store', [UserController::class, 'store'])->name('user.store');
